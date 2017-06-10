7fa8a849fad82835cf5ac472d48ce813bd1219cf
Lucas Sabião <lsabiao@users.noreply.github.com>
2017-06-09 22:57:22 -0300

    herald.py is now arauto.py
    now arauto is a class.

--- arauto.py
+++ arauto.py
@@ -3,14 +3,75 @@ import datetime
 import mimetypes
 import os
 
-version = "1.02"
+#TODO migrar para o python3
+
+version = "1.10"
 
 mimetypes.init()
 
 #mimetypes not supported in default python
 mimetypes.add_type("application/font-woff2",".woff2")
 
-class response():
+class Arauto():
+    
+    def __init__(self,port=8088):
+        self.host = ""
+        self.port = port 
+
+    def run(self):
+        #python's socket magic
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind((self.host,self.port))
+        self.s.listen(10)
+
+        self.ip = socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))
+        self.ip = self.ip[2][0]
+
+
+        #TODO modo silencioso
+        print "*Hello, Master*"
+        print "Serving at: {ip}:{port}".format(ip = self.ip, port = self.port)
+        print
+
+        while True:
+            #TODO multi-threaded server?
+            self.conn, self.addr = self.s.accept()
+            self.data = self.conn.recv(1024)
+
+            try:
+                #get the file name from the URL
+                self.fileRequested = self.getUrl(self.data)
+                
+                #create a response object
+                self.c = Response(self.addr)
+                
+                #add a file to the response
+                self.c.addFile(self.fileRequested)
+
+                #send the file as a string
+                self.conn.sendall(str(self.c))
+                self.conn.close()
+            except:
+                self.conn.close()
+        
+
+    def getUrl(self,url):
+        #get the file name and path from the get URL
+        url = url.split("\r\n")[0]
+        arg = url.split(" ")[1]
+        arg = arg.lstrip("/")
+        if(arg == ""):
+            arg = "index.html"
+            return arg
+        if("." not in arg):
+            arg= arg.rstrip("/")
+            arg+="/index.html"
+            return arg
+        return arg
+
+
+class Response():
     '''
         the response class
         for every request, we need a response
@@ -35,12 +96,13 @@ class response():
         self.status      = 200
         self.location    = socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))[0]
         self.date        = datetime.datetime.now()
-        self.serverName  = "herald-"+version
+        self.serverName  = "Arauto-"+version
         self.contentType = ""
         self.contentLen  = ""
         self.connection  = "close"
         self.payload     = ""
         self.addr        = addr
+
     def __str__(self):
         return self.template.format(protocol = self.protocol,
                                     status = self.status,
@@ -77,59 +139,12 @@ class response():
         finally:
             print "{time} - File: '{file}' requested by: {addr} {status}".format(time = datetime.datetime.now(),file=file,status=stat,addr=self.addr[0])
             
-def getURL(url):
-    #get the file name and path from the get URL
-    url = url.split("\r\n")[0]
-    arg = url.split(" ")[1]
-    arg = arg.lstrip("/")
-    if(arg == ""):
-        arg = "index.html"
-        return arg
-    if("." not in arg):
-        arg= arg.rstrip("/")
-        arg+="/index.html"
-        return arg
-    return arg
-    
-def main(host="",port=8088):
-    #python's socket magic
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host,port))
-    s.listen(10)
-
-    ip = socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))
-    ip = ip[2][0]
-    
-    print "**Hello, Master**"
-    print "Serving at: {ip}:{port}".format(ip= ip , port=port)
-    print 
-
-    #application main loop
-    while True:
-        conn, addr = s.accept()
-        data = conn.recv(1024)
-        try:
-            #get the file name from the URL
-            fileRequested = getURL(data)
-
-            #create a response object
-            c = response(addr)
-            #add a file to the response
-            c.addFile(fileRequested)
-
-            #send the file as a string.
-            conn.sendall(str(c))
-            conn.close()
-        except:
-            conn.close()
-            
-
 if __name__ == "__main__":
     import sys
     try:
         newPort = int(sys.argv[1])
-        main(port = newPort)
+        s = Arauto(port = newPort)
     except:
         print "[You need admin permissions to run this port, using 8088]"
-    main()
+    s = Arauto()
+    s.run()
